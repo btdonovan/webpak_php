5e478e9a468979218defe8195ebfc9193e0e4e7f
Benjamin Donovan <carcel@navonod.us>
2024-04-28 21:56:50 -0600

    Following webpack through build-performance

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,11 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Output Management</title>
+        <script src="./dist/print.bundle.js"></script>
+    </head>
+    <body>
+        <script src="./dist/index.bundle.js"></script>
+    </body>
+</html>
---
+++ app/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Caching',
+        }),
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'public', 'dist'),
+        clean: true,
+    },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+};
